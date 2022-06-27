158d96583a83e0f0440c5377252bc8ac5ac4fee2
Krishna Dhungel <dhungelkrishna807@gmail.com>
2022-06-27 22:46:09 +0545

    Case Added

--- OOP.java
+++ OOP.java
@@ -3,7 +3,18 @@ public class OOP {
     public static void main(String [] args) {
         System.out.println("Welcome to Loralai Shopping Mall.");
         Scanner sn = new Scanner(System.in);
-        char a = sn.next().charAt(0);
-        System.out.println("YOur Entered String is :" + a);
+        char choice = sn.next().charAt(0);
+        System.out.println("YOur Entered String is :" + choice);
+        switch (choice) {
+            case 'a':
+            System.out.println("Welcome in Electronics Branch.");
+            break;
+            case 'b':
+            System.out.println("Welcome in the Mobile Shop.");
+            break;
+            case 'x':
+            break;
+            // exit(1);
+        }
     }
 }
