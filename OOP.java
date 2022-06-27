3ba3a39a1d2f4bb268cbbde92ed5d86a29b8f467
Krishna Dhungel <dhungelkrishna807@gmail.com>
2022-06-27 08:52:05 +0545

    Second Commit After change

--- OOP.java
+++ OOP.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class OOP {
     public static void main(String [] args) {
         System.out.println("Welcome to Loralai Shopping Mall.");
+        Scanner sn = new Scanner(System.in);
+        char a = sn.next().charAt(0);
+        System.out.println("YOur Entered String is :" + a);
     }
 }
