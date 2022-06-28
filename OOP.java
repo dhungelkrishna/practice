43374d85b357bc71ded1ae50abdc4dcd40ad1805
Krishna Dhungel <dhungelkrishna807@gmail.com>
2022-06-28 08:14:57 +0545

    changes 6-28

--- OOP.java
+++ OOP.java
@@ -11,10 +11,40 @@ public class OOP {
             break;
             case 'b':
             System.out.println("Welcome in the Mobile Shop.");
-            break;
+            System.out.println("Which Mobile do you want? ");
+            String mobile = sn.next();
+            switch(mobile) {
+                System.out.println("Enter 'y' to buy phone or Enter 'n' to Exit from it. ");
+                Char input = sn.next().charAt();
+                case "samsung" :
+                System.out.println("There is only one Mobile in the Database. Samsung.");
+                yesno(input);
+                break;
+                case "iphone" :
+                System.out.println("There is Iphone in the Database.");
+                yesno(input);
+            }
+
             case 'x':
             break;
             // exit(1);
         }
+
+    }
+    Char y = 'x';
+    static void yesno(y) {
+        if(y == 'y') {
+            System.out.println("Bought Successfully.");
+            break;
+        }
+        if(y = 'n') {
+            System.out.println("Okay, you're Returning.");
+            break;
+        }
+    }
+    void returnfunc() {
+        System.out.println("Hello Progaram.");
     }
 }
+// 9741862183  driver 
+// 3264 dolakha darshan
